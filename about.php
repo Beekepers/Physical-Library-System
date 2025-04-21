3d884e2b31e5339b9c35cbf814569e3d1174b0a3
Beekepers <adriannoveda2@gmail.com>
2025-04-21 10:13:22 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,180 @@
+<?php
+
+require_once 'core/init.php';
+
+$user = new UserLogin(); //Current
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>ABOUT US</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/fontawesome-5.0.8/css/fontawesome-all.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+	<style>
+	blockquote {  
+		border-style: solid;
+		border-width: 5px;
+		font-size: 15px;
+		font-style: italic;
+		border-color: #eee #17b978;
+		border-left: 5px solid #17b978;
+		padding:20px;
+	}
+	</style>
+</head>
+<body class="animsition">
+	
+	<!-- Header -->
+	<header>
+		<!-- Header desktop -->
+		<div class="container-menu-desktop">
+			
+			<?php include 'includes/top-header.php'; ?>
+			<!--  -->
+			<?php include 'includes/wrap-main-nav.php'; ?>	
+		</div>
+	</header>
+
+	<!-- Breadcrumb -->
+	<div class="container">
+		<div class="headline bg0 flex-wr-sb-c p-rl-20 p-tb-8">
+			<div class="f2-s-1 p-r-30 m-tb-6">
+				<a href="index.php" class="breadcrumb-item f1-s-3 cl9">
+					Home 
+				</a>
+
+				<span class="breadcrumb-item f1-s-3 cl9">
+					About Us
+				</span>
+			</div>
+		</div>
+	</div>
+
+	<!-- Page heading -->
+	<div class="container p-t-4 p-b-40">
+		<h2 class="f1-l-1 cl2">
+			ABOUT US
+		</h2>
+	</div>
+
+	<!-- Content -->
+	<section class="bg0 p-b-60">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-7 col-lg-8 p-b-30">
+					<div class="p-r-10 p-r-0-sr991">
+						<p class="f1-s-11 cl6 p-b-25">
+						
+Established on September 20, 2002, Marie-Bernarde College Inc. (MBC) was founded by a visionary group of nursing educators, esteemed medical practitioners, and experts in business and education. From its inception, MBC has been dedicated to providing an exceptional, globally competitive healthcare education. The college’s early years were marked by a dynamic start and a commitment to growth, beginning with the introduction of a comprehensive 6-month Caregiver Course. This course, expertly designed and programmed by trusted professionals, set the foundation for MBC’s reputation for excellence in healthcare education.
+
+Building on this strong foundation, MBC expanded its offerings to include a range of TESDA-registered and accredited courses, delivered on a trimester basis over two years. These courses encompass Basic Nursing Assistant, Business Management and Information Technology, Business Computing and Accountancy, Health Care Information System, and Computer and Network Design. Each program is meticulously crafted to meet industry standards and prepare students for the demands of their chosen fields. MBC’s commitment to innovation and relevance in its curriculum ensures that graduates are well-equipped to thrive in a competitive global market.
+
+ Today, Marie-Bernarde College Inc. proudly offers a diverse array of degree programs, including Bachelor of Science in Nursing, Bachelor of Business Administration with majors in Human Resource Management and Marketing Management, Bachelor of Secondary Education with majors in English, Science, and Mathematics, and a Diploma in Midwifery. Our goal is to produce highly skilled and globally competitive professionals who exceed industry standards. Committed to excellence, MBC is dedicated to expanding our offerings in health and sciences, ensuring our students receive the best education and training available. By continually evolving and adapting to the needs of the healthcare and business sectors, MBC remains at the forefront of educational excellence.
+						</p>
+
+						<p class="f1-s-11 cl6 p-b-25">
+						</p>
+
+						<p class="f1-s-11 cl6 p-b-25">
+						</p>
+						
+							
+						
+						<blockquote>
+							<b class="text-danger">VISION</b><hr>
+							<p>Marie-Bernarde College Inc., formerly Saint Bernadette College of Health & Sciences, envisions itself as the leading provider of quality education in nursing, education, business and management, and diverse professional fields. We strive to be the dominant force in delivering exceptional education, fully responsive to the needs and demands of both local and global markets. Committed to educational excellence and spiritual growth, we aim to set the standard for academic and professional achievement, preparing our students to be forward thinkers, leaders, and innovators in their respective fields.</p>
+						</blockquote>
+						<blockquote>
+							<b class="text-danger">MISSION</b><hr>
+							<p>Marie-Bernarde College Inc., formerly Saint Bernadette College of Health & Sciences, is dedicated to providing outstanding education in nursing, education, business and management, and various other fields. We are committed to continually improving the skills and knowledge of our educators, staff, and students to meet the evolving needs of both local and global markets. Guided by Divine Providence, MBC serves the youth, the nation, and God. We champion academic freedom, innovative teaching, creative scholarship, and international goodwill, ensuring our students emerge as leaders, innovators, and forward thinkers in their fields.</p>   
+						</blockquote> 
+					</div>
+				</div>
+				<!-- Sidebar -->
+				<div class="col-md-5 col-lg-4 p-b-30">
+					<div class="p-l-10 p-rl-0-sr991 p-t-5">
+						<!-- Popular Posts -->
+						<div>
+							<div class="how2 how2-cl4 flex-s-c">
+								<h3 class="f1-m-2 cl3 tab01-title">
+									Popular Post
+								</h3>
+							</div>
+
+							<ul class="p-t-35">
+								<li class="flex-wr-sb-s p-b-30">
+									<a href="#" class="size-w-10 wrap-pic-w hov1 trans-03">
+										<img src="images/history.jpg" alt="IMG">
+									</a>
+
+									<div class="size-w-11">
+										<h6 class="p-b-4">
+											<a href="#" class="f1-s-5 cl3 hov-cl10 trans-03">
+											
+											</a>
+										</h6>
+
+										<span class="cl8 txt-center p-b-24">
+											<a href="#" class="f1-s-6 cl8 hov-cl10 trans-03">
+											</a>
+
+											<span class="f1-s-3 m-rl-3">
+											</span>
+
+											<span class="f1-s-3">
+											</span>
+										</span>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<!-- Footer -->
+	<?php include 'includes/footer.html'; ?>
+
+	<!-- Back to top -->
+	<div class="btn-back-to-top" id="myBtn">
+		<span class="symbol-btn-back-to-top">
+			<span class="fas fa-angle-up"></span>
+		</span>
+	</div>
+
+
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ admin/dashboard.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- bootstrap 3.0.2 -->
+<link href="styles/admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<!-- font Awesome -->
+<link href="styles/admin/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+<link href="styles/admin/css/all.css" rel="stylesheet" type="text/css" />
+<!-- Ionicons -->
+<link href="styles/admin/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+<!-- bootstrap wysihtml5 - text editor -->
+<link href="styles/admin/css/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css" rel="stylesheet" type="text/css" />
+<!-- Theme style -->
+<link href="styles/admin/css/AdminLTE.css" rel="stylesheet" type="text/css" />
+<!-- DATA TABLES -->
+<link href="styles/admin/css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+<SCRIPT LANGUAGE="Javascript" SRC="styles/admin/js/FusionCharts.js"></SCRIPT>
+<title>SAN PEDRO</title>
+</head>
+<body class="skin-blue">
+        <!-- header logo: style can be found in header.less -->
+        <?php include_once('navigation.php'); ?>
+
+            <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Dashboard
+                        <small>Control panel</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li class="active">Home or Dashboard</li>
+                    </ol>
+                </section>
+
+               <h1> Welcome Administrator
+                    
+<!-- jQuery 2.0.2 -->
+<script src="styles/admin/js/jquery.min.js"></script>
+<!-- jQuery UI 1.10.3 -->
+<script src="styles/admin/js/jquery-ui-1.10.3.min.js" type="text/javascript"></script>
+<!-- Bootstrap -->
+<script src="styles/admin/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="styles/admin/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>
+<!-- DATA TABES SCRIPT -->
+<script src="styles/admin/js/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+<script src="styles/admin/js/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="styles/admin/js/AdminLTE/app.js" type="text/javascript"></script>
+<!-- page script -->
+</body>
+
+</html>
